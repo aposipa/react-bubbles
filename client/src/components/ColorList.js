cbd3607bcd15e4c9fb99cbd6525698d6e766d8f7
aposipa <andrew.posipanka@gmail.com>
2020-03-27 13:33:25 -0400

    MVP Complete.

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWIthAuth";
 
 const initialColor = {
   color: "",
@@ -18,12 +19,34 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
+    axiosWithAuth()
+    .put(`/colors/${colorToEdit.id}`, colorToEdit)
+    .then(res => {
+      axiosWithAuth()
+      .get('/colors')
+      .then(res => {
+        updateColors(res.data)
+        setEditing(false);
+      })
+      .catch(err => console.log("axios put request error", err))
+    })
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
   };
 
   const deleteColor = color => {
+    axiosWithAuth()
+    .delete(`/colors/${color.id}`)
+    .then(res => {
+      axiosWithAuth()
+      .get('/colors')
+      .then(res => {
+        updateColors(res.data)
+      })
+      .catch(err => console.log('axios delete request error', err))
+    })
+
     // make a delete request to delete this color
   };
 
